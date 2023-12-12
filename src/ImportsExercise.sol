ba5f90657713cfed9248f61540a4d5df8fcfcb56
brianjckim <brian@brianjckim.com>
2023-12-12 14:10:48 -0500

    switch to foundry

--- src/ImportsExercise.sol
+++ src/ImportsExercise.sol
@@ -1,7 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import "./SillyStringUtils.sol";
+library SillyStringUtils {
+    struct Haiku {
+        string line1;
+        string line2;
+        string line3;
+    }
+
+    function shruggie(string memory _input) internal pure returns (string memory) {
+        return string.concat(_input, unicode" 🤷");
+    }
+}
 
 contract ImportsExercise {
     SillyStringUtils.Haiku public haiku;
